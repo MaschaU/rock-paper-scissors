332beca3843f3f7e245da6161378dd1c202870b7
Masa Utkovic <masa.utkovic@gmail.com>
2020-12-29 12:12:06 +0100

    it works

--- index.js
+++ index.js
@@ -1,59 +1,90 @@
-console.log("sanity check");
-
+// console.log("sanity check");
 const message = document.querySelector(".message");
-const buttons = document.querySelectorAll(".button");
 const score = document.querySelector(".score");
+const buttons = document.querySelectorAll("button");
+const scoreTracker = [0,0];
 
-// adding event listeners to buttons
-
+//add event listeners to buttons
 for ( let i = 0 ; i < buttons.length ; i++){
-    buttons[i].addEventListener('click', play);
+    // console.log("Clicking the shite out of it");
+    buttons[i].addEventListener("click", play);
 }
 
-
-
-
-function play(e) {
+function play(e){
     // set up players selection
-    // set up random number as computer's choice
+    let playersChoice = e.target.innerText;
+    // set up random number as computers selection
+    let computersChoice = Math.random();
+
     // if computer's number < .34, Rocks
     // if computer's number <= .67, Paper
     // else Scisssors
+    if (computersChoice < .34){
+        computersChoice = "Rock";
+    } else if (computersChoice <= .67){
+        computersChoice = "Paper";
+    } else {
+        computersChoice = "Scissors";
+    }
+    
+    // just cover all the possible combinations
+    function checkForWinner(human, machine){
+        if (human === machine){
+            return "Draw";
+        }
+    
+        if (human === "Rock"){
+            if(machine === "Paper"){
+                return "Machine";
+            } else {
+                return "Human";
+            }
+        }
+    
+        if (human === "Paper"){
+            if (machine === "Scissors"){
+                return "Machine";
+            } else {
+                return "Human";
+            }
+        }
+    
+        if (human === "Scissors"){
+            if (machine === "Rock"){
+                return "Machine";
+            } else {
+                return "Human";
+            }
+        }
+    }
+    
     // set up a function to compare the results
+    let result = checkForWinner(playersChoice, computersChoice);
     // add scores to DOM
-    // if (result === "Player"){
-    //     result += "wins!!!"
-    //     winnerScores[0]++;
-    // }
+    if (result === "Human"){
+        result += " takes the cake!";
+        scoreTracker[0]++;
+    }
+
+    if (result === "Machine"){
+        result += " is coming to get you!";
+        scoreTracker[1]++;
+    }
+
+    if (result === "Draw"){
+        result += ". We are all losers!"
+    }
+
     // put score into score div
+    score.innerHTML = "Human: [ " + scoreTracker[0]+ " ] Machine: [ " + scoreTracker[1] + " ]";
 
+    // player & computer selection output
+    function displayMessage(selectionMessage){
+    message.innerHTML = selectionMessage;
+    }
+
+    displayMessage("Human: " + playersChoice + " Machine: " + computersChoice + "<br>" + result);
 }
 
-function checkForWinner(player, computer) {
-    if (player === computer) {
-        return "It's a draw";
-    }
-    if (player === "Rock"){
-        if(computer === "Paper"){
-            return "Computer wins";
-        } else {
-            return "Player wins";
-        }
-    }
 
-    if (player === "Paper"){
-        if (computer === "Scissors"){
-            return "Computer wins";
-        } else {
-            return "Player wins";
-        }
-    }
 
-    if (player === "Scissors"){
-        if (computer === "Rock"){
-            return "Computer wins";
-        } else {
-            return "Player wins";
-        }
-    }
-}
